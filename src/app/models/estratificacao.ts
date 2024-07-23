af51a42107070cb8647694d14317090a9f913cf0
Lucas Cassimiro Calzavara <lucascalzavara95@gmail.com>
2024-07-23 01:46:04 -0300

    adicionado modal de intrucoes e correcoes

--- src/app/models/estratificacao.ts
+++ src/app/models/estratificacao.ts
@@ -2,19 +2,20 @@ import { Grupo } from "./grupo";
 
 export class Estratificacao{
     nomeUsuario: string = ''
-    grupos: Grupo[] = [];
     dataNascimento?: Date;
     telefone: string = '';
-    nomeInscricaoProficional: string = '';
-    municipio: string = '';
-    cartaoSus: string = '';
-    servicoSaude: string = '';
-    data?: Date;
-    referencia: string = '';
-    contrarreferencia: string = '';
     endereco: string = '';
     numero: string = '';
     complemento: string = '';
+    servicoSaude: string = '';
+    servicoSaudeEspecifico: string = '';
+    municipio: string = '';
+    cartaoSus: string = '';
+    nomeInscricaoProficional: string = '';
+    informacoesAdicionais: string = '';
+    
+    data?: Date;
+    grupos: Grupo[] = [];
 
     total: number = 0;
     resultado: string = '';
